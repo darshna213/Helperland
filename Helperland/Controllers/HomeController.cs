7a6b134e4e68139c4aee6e1526097b5e1520921c
darshna213 <93382999+darshna213@users.noreply.github.com>
2022-02-02 19:50:42 +0530

    files upload

---
+++ Helperland/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Helperland.Models;
+using Helperland.Data;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace Helperland.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        private readonly HelperlandContext _helperlandContext;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public HomeController(HelperlandContext helperlandContext, IWebHostEnvironment webHostEnvironment)
+        {
+            _helperlandContext = helperlandContext;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Faq()
+        {
+            return View();
+        }
+        public IActionResult Price()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            ContactU contactU = new ContactU();
+            return View(contactU);
+           
+        }
+
+        [HttpPost]
+
+
+        public IActionResult Contact(ContactU contactU)
+        {
+            if (contactU.AttechmentFile != null)
+            {
+                string folder = "ContactUsFiles/";
+                folder += Guid.NewGuid().ToString() + "_" + contactU.AttechmentFile.FileName;
+                string serverFolder = Path.Combine(_webHostEnvironment.WebRootPath, folder);
+                contactU.AttechmentFile.CopyToAsync(new FileStream(serverFolder, FileMode.Create));
+                contactU.FileName = folder;
+
+            }
+
+            contactU.CreatedOn = DateTime.Now;
+            _helperlandContext.ContactUs.Add(contactU);
+            _helperlandContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Provider()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Helperland/Models/ContactU.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace Helperland.Models
+{
+    public partial class ContactU
+    {
+        public int ContactUsId { get; set; }
+        //public string FirstName { get; set; }
+        //public string LastName { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Message { get; set; }
+        public string UploadFileName { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public int? CreatedBy { get; set; }
+        public string FileName { get; set; }
+
+
+        [Display(Name = "Attech the file")]
+        [Required]
+        [NotMapped]
+        public IFormFile AttechmentFile { get; set; }
+
+    }
+}
+

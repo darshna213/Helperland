c0bc3d8a408d9691c472b347de54a8b29ea062c3
darshna213 <93382999+darshna213@users.noreply.github.com>
2022-02-03 20:00:29 +0530

    files upload

--- Helperland/Controllers/AccountController.cs
+++ Helperland/Controllers/AccountController.cs
@@ -2,15 +2,76 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Helperland.Data;
+using Helperland.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Helperland.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private readonly HelperlandContext _helperlandContext;
+        public AccountController(HelperlandContext helperlandContext)
+        {
+            _helperlandContext = helperlandContext;
+        }
+        public IActionResult Signup()
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult Signup(User user)
+        {
+
+            if (_helperlandContext.Users.Where(x => x.Email == user.Email).Count() == 0 && _helperlandContext.Users.Where(x => x.Mobile == user.Mobile).Count() == 0)
+            {
+                user.UserTypeId = 1;
+                user.CreatedDate = DateTime.Now;
+                user.ModifiedDate = DateTime.Now;
+                user.IsRegisteredUser = true;
+                user.ModifiedBy = 123;
+
+                _helperlandContext.Users.Add(user);
+                _helperlandContext.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.message = "User already exist.";
+            }
+            return View();
+
+        }
+        public IActionResult Provider()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Provider(User signup)
+        {
+
+            if (_helperlandContext.Users.Where(x => x.Email == signup.Email).Count() == 0 && _helperlandContext.Users.Where(x => x.Mobile == signup.Mobile).Count() == 0)
+            {
+                //signup.UserTypeId = 2;
+                signup.CreatedDate = DateTime.Now;
+                signup.ModifiedDate = DateTime.Now;
+                signup.IsRegisteredUser = true;
+                signup.ModifiedBy = 123;
+
+                _helperlandContext.Users.Add(signup);
+                _helperlandContext.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.message = "User already exist.";
+            }
+            return View();
+        }
+
     }
 }
